04722c57b4f5c935d5ea284327fd37f795e4c30f
Jelle De Boeck <jelle.deboeck@axxes.com>
2026-08-24 22:18:59 +0200

    Remove unused terraform modules not yet introduced at this checkpoint

--- terraform/ecr/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "repository_url" {
-  description = "Full URL of the ECR repository (used in docker tag/push commands)."
-  value       = aws_ecr_repository.this.repository_url
-}
-
-output "repository_name" {
-  description = "Name of the ECR repository."
-  value       = aws_ecr_repository.this.name
-}
